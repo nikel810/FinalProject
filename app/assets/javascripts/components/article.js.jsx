3ba213dbb59b73a4c9573101e8b48087a7b963bc
nikel810 <niklas.reinhardt@gmx.de>
2016-08-01 22:32:25 +0200

    added link to articles

--- app/assets/javascripts/components/article.js.jsx
+++ app/assets/javascripts/components/article.js.jsx
@@ -6,10 +6,12 @@ var Article = React.createClass({
         <div className="article-box">
           <div className="article-heading">
             <div className="article-img-container">
-              <img className="article-img" src={this.props.img} alt="thumbnail" />
+              <a href={this.props.url}>
+                <img href={this.props.url} className="article-img" src={this.props.img} alt="thumbnail" />
+              </a>
             </div>
             <div className="article-title">
-              {this.props.title}
+              <a className="react-links" href={this.props.url}>{this.props.title}</a>
             </div>
           </div>
           <div className="article-description">
