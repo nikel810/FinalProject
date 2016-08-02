c2e65796de25adfab8b77a85db48dd85ee1f3ae1
nikel810 <niklas.reinhardt@gmx.de>
2016-08-03 01:41:39 +0200

    added like button in views

--- app/assets/javascripts/components/article.js.jsx
+++ app/assets/javascripts/components/article.js.jsx
@@ -4,6 +4,7 @@ var Article = React.createClass({
     return(
       <div className="article-padding-background">
         <div className="article-box">
+
           <div className="article-heading">
             <div className="article-img-container">
               <a href={this.props.url}>
@@ -14,9 +15,18 @@ var Article = React.createClass({
               <a className="react-links" target="_blank" href={this.props.url}>{this.props.title}</a>
             </div>
           </div>
+
           <div className="article-description">
             {this.props.description}
           </div>
+
+          <div className="article-reaction">
+            <span className="glyphicon glyphicon-thumbs-up thumbs-up" aria-hidden="true"></span>
+            <span className="thumbs-up-count">00</span>
+            <span className="glyphicon glyphicon-thumbs-down thumbs-down" aria-hidden="true"></span>
+            <span className="thumbs-down-count">00</span>
+          </div>
+
         </div>
       </div>
     );
--- app/assets/javascripts/components/timeline-article.js.jsx
+++ app/assets/javascripts/components/timeline-article.js.jsx
@@ -4,22 +4,34 @@ var TimelineArticles = React.createClass({
     return(
       <div className="article-padding-background">
         <div className="article-box">
+
           <div className="article-upload-info">
             <span>Posted by {this.props.name} {this.props.surname} </span>
           </div>
+
           <div className="article-heading">
             <div className="article-img-container">
               <a href={this.props.url}>
                 <img href={this.props.url} className="article-img" target="_blank" src={this.props.img} alt="thumbnail" />
               </a>
             </div>
+
             <div className="article-title">
               <a className="react-links" target="_blank" href={this.props.url}>{this.props.title}</a>
             </div>
           </div>
+
           <div className="article-description">
             {this.props.description}
           </div>
+
+          <div className="article-reaction">
+            <span className="glyphicon glyphicon-thumbs-up thumbs-up" aria-hidden="true"></span>
+            <span className="thumbs-up-count">00</span>
+            <span className="glyphicon glyphicon-thumbs-down thumbs-down" aria-hidden="true"></span>
+            <span className="thumbs-down-count">00</span>
+          </div>
+          
         </div>
       </div>
     );
